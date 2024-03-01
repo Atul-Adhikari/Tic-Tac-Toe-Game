8ba519065944e9ddf37c81e7d7dc3b58ae09f601
Atul Adhikari <143103714+Atul-Adhikari@users.noreply.github.com>
2024-03-01 15:21:26 +0545

    Add files via upload

---
+++ Tic tac toe/play_game.py
@@ -0,0 +1,38 @@
+"""
+    Tic Tac Toe Game
+    Student Name: Atul Adhikari
+    Student ID: 2408982
+"""
+
+from noughts_and_crosses import *
+
+
+def main():
+    board = [ ['1','2','3'],\
+              ['4','5','6'],\
+              ['7','8','9']]
+
+    welcome(board)
+    total_score = 0
+    while True:
+        choice = menu()
+        if choice == '1':
+            score = play_game(board)
+            total_score += score
+            print('Your current score is:',total_score)
+        if choice == '2':
+            save_score(total_score)
+            total_score=0;
+        if choice == '3':
+            leader_board = load_scores()
+            display_leaderboard(leader_board)
+        if choice == 'q':
+            print('Thank you for playing the "Unbeatable Noughts and Crosses" game.')
+            print('Good bye')
+            return
+        if choice not in ['1','2','3','q']:
+            print("Please select valid option.")
+
+# Program execution begins here
+if __name__ == '__main__':
+    main()
